a9f7d242f19f0254a5c8fbb5034fb3650a8ed647
Scotty <scott.hagan@aol.com>
2017-09-20 20:31:11 +0000

    added loadTweets funtion to load tweets using AJAX get after a new tweet is ssubmitted

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,60 +3,10 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-var data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: {
-        small: "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        regular: "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        large: "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      handle: "@SirIsaac"
-    },
-    content: {
-      text:
-        "If I have seen further it is by standing on the shoulders of giants"
-    },
-    created_at: 1461116232227
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: {
-        small: "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        regular: "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        large: "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      handle: "@rd"
-    },
-    content: {
-      text: "Je pense , donc je suis"
-    },
-    created_at: 1461113959088
-  },
-  {
-    user: {
-      name: "Johann von Goethe",
-      avatars: {
-        small: "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        regular: "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        large: "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      handle: "@johann49"
-    },
-    content: {
-      text: "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    created_at: 1461113796368
-  }
-];
 
 $(function() {
   function createTweetElement(tweetObj) {
     const $article = $("<article>").addClass("tweet");
-    $("#tweets-container").append($article);
-
     const $img = $("<img>").attr("src", tweetObj.user.avatars.small);
     const $h1 = $("<h1>").text(tweetObj.user.name);
     const $userHandle = $("<p>").text(tweetObj.user.handle);
@@ -85,17 +35,32 @@ $(function() {
       .append($favIcon);
 
     $article.append($footer);
+
+    return $article;
   }
+
+  $("form").on("click", "input", function(event) {
+    event.preventDefault();
+    const tweetContent = $("form").serialize();
+    $.post("/tweets", tweetContent, function(result) {
+      $("textarea").val("");
+      loadTweets();
+    });
+  });
 
   function loadTweets() {
-    $.get("/tweets");
+    $.get("/tweets", function(tweets) {
+      renderTweets(tweets);
+    });
   }
 
+  loadTweets();
+
   function renderTweets(tweets) {
+    $("#tweets-container").empty();
     tweets.forEach(tweet => {
-      createTweetElement(tweet);
+      $("#tweets-container").prepend(createTweetElement(tweet));
     });
     return tweets;
   }
-  renderTweets(data);
 });
